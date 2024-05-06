57935cda720aeedba3e516cefd0c2c6fc08b0cac
KenanOek <kenanoksoy@gmail.com>
2024-05-06 14:40:49 +0200

    Testload

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Jeff Besoz",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Elon Musk",
+        "right_answer": 3
+    },
+    {
+        "question": "Wann endete der 2. Weltkrieg ?",
+        "answer_1": "1943",
+        "answer_2": "1928",
+        "answer_3": "1945",
+        "answer_4": "1944",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie hieß der Gründer von Apple ?",
+        "answer_1": "Tom Cruise",
+        "answer_2": "Steve Jobs",
+        "answer_3": "Clint Eastwood",
+        "answer_4": "Akira Toriyama",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie lang ist die Chinesische Mauer ?",
+        "answer_1": "9.000 km",
+        "answer_2": "16.000 km",
+        "answer_3": "32.000 km",
+        "answer_4": "21.000 km",
+        "right_answer": 4
+    },
+    {
+        "question": "Welches ist keine Programmiersprache ?",
+        "answer_1": "HTML",
+        "answer_2": "C++",
+        "answer_3": "Phyton",
+        "answer_4": "Swift",
+        "right_answer": 1
+    }
+];
